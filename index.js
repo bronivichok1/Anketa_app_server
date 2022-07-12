a30ecc06c6635bacddbeeec0bf4cb781de4aa94b
mashalin <mashatrushel666@gmail.com>
2022-07-12 13:10:12 +0300

    change json limit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '1024mb', parameterLimit: 10000000, extended: true }));
+app.use(express.json({limit: '20mb', parameterLimit: 20000, extended: true }));
 app.use('/api', router);
 
 app.use(errorHandler);
